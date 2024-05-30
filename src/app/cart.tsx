4aec16b817a73d3d87d8ca059dc5f7ab6f476f8d
cypriel17 <cypriel17@gmail.com>
2024-05-30 20:51:28 +0200

    shopping cart updated

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,13 +1,29 @@
+import { View, Text, Platform, FlatList } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, Platform } from 'react-native';
+
+import { useCart } from '../provider/CartProvider';
+import CartListItem from '../components/CartListItem';
+import Button from '../components/Button';
 
 const CartScreen = () => {
-    return (
-        <View>
-            <Text>cart</Text>
-            <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-        </View>
-    )
-}
+  const { items, total, checkout } = useCart();
+
+  return (
+    <View style={{ padding: 10 }}>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        contentContainerStyle={{ gap: 10 }}
+      />
+
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: '500' }}>
+        Total: R{total}
+      </Text>
+      <Button onPress={checkout} text="Checkout" />
+
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    </View>
+  );
+};
 
 export default CartScreen;
