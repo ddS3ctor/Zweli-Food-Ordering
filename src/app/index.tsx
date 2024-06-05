c0a6cb672c7298b64d91198ba15572f0798cfe14
cypriel17 <cypriel17@gmail.com>
2024-06-05 13:13:15 +0200

    basic authentication flow for the user - user rules

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,24 @@
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import React from 'react';
 import Button from '../components/Button';
-import { Link } from 'expo-router';
+import { Link, Redirect } from 'expo-router';
+import { useAuth } from '@/provider/AuthProvider';
+import { supabase } from '@/lib/supabase';
 
 const index = () => {
+
+  const { session, loading } = useAuth();
+
+  if (loading){
+    return <ActivityIndicator />
+  }
+
+  if (!session) {
+    return <Redirect href={'/sign-in'} />;
+  }
+
+
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
       <Link href={'/sign-in'} asChild>
@@ -17,6 +32,8 @@ const index = () => {
       <Link href={'/(admin)'} asChild>
         <Button text="Admin" />
       </Link>
+
+      <Button onPress={() => supabase.auth.signOut()} text='Sign out' />
     </View>
   );
 };
